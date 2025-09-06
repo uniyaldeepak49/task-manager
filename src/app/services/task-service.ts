ebb213034986ce5572a6107c2a34d20a0a4f2437
Deepak Uniyal <uniyaldeepak49@gmail.com>
2025-09-06 19:05:01 +0530

    Implementation on part 4 as per Angular ToDo App RoadMap

---
+++ src/app/services/task-service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../interfaces/task';
+import { TASK_LIST } from '../task-list/task-test-data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  /**
+   * Returns task list
+   * @returns {Task[]}
+   */
+  getTasks(): Task[] {
+    const tasks: Task[] = TASK_LIST;
+    return tasks;
+  }
+  /**
+   * Delete the task and returns the refined task list.
+   * @param task
+   * @returns {Task[]}
+   */
+  deleteTask(id: number): Task[] {
+    const tasks: Task[] = this.getTasks();
+    const refinedTasks: Task[] = tasks.filter((t: Task) => t.id !== id);
+    return refinedTasks;
+  }
+  addTask(): void {}
+  editTask(): void {}
+}
--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TASK_LIST } from './task-test-data';
 import { Status, Task } from '../interfaces/task';
 import { CommonModule } from '@angular/common';
+import { TaskService } from '../services/task-service';
 
 @Component({
   selector: 'code-for-beginners-task-list',
@@ -12,17 +12,28 @@ import { CommonModule } from '@angular/common';
 })
 export class TaskList {
   taskTitle: string = 'Task title 1';
-  tasks: Task[] = TASK_LIST;
+  tasks: Task[] = [];
   status = Status;
+  private taskService = inject(TaskService);
+  // private taskService: TaskService
+  // @Inject(TaskService) private taskService: TaskService;
 
+  constructor() {
+    this.tasks = this.taskService.getTasks();
+  }
   /**
    * Deletes the task from task list by ID.
    * @param id
    * @returns {void}
    */
   deleteTask(id: number): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.tasks = this.taskService.deleteTask(id);
   }
+  /**
+   * On Change mark task completed.
+   * @param task
+   *
+   */
   onTaskComplete(task: Task): void {
     if (task.status === Status.PENDING) {
       task.status = Status.COMPLETED;
