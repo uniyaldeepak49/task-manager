6b9ed08914261421265be1cdb62ee26db7de0a59
Deepak Uniyal <uniyaldeepak49@gmail.com>
2025-09-06 12:23:53 +0530

    Implementation of Part 3 as per Angular ToDo Roadmap doc.

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TASK_LIST } from './task-test-data';
-import { Task } from '../interfaces/task';
+import { Status, Task } from '../interfaces/task';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'code-for-beginners-task-list',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.html',
   styleUrls: ['./task-list.css'],
 })
 export class TaskList {
   taskTitle: string = 'Task title 1';
   tasks: Task[] = TASK_LIST;
+  status = Status;
 
   /**
    * Deletes the task from task list by ID.
@@ -20,5 +22,12 @@ export class TaskList {
    */
   deleteTask(id: number): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  onTaskComplete(task: Task): void {
+    if (task.status === Status.PENDING) {
+      task.status = Status.COMPLETED;
+    } else {
+      task.status = Status.PENDING;
+    }
   }
 }
