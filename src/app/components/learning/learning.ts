ffd97bbaee67d9959bbf377ac5e982db7bbed048
Deepak Uniyal <uniyaldeepak49@gmail.com>
2025-09-17 12:38:03 +0530

    Formatted the code

--- src/app/components/learning/learning.ts
+++ src/app/components/learning/learning.ts
@@ -5,23 +5,17 @@ import { from, Observable, of } from 'rxjs';
   selector: 'code-for-beginners-learning',
   imports: [],
   templateUrl: './learning.html',
-  styleUrl: './learning.css'
+  styleUrl: './learning.css',
 })
 export class Learning {
-
   // Blocking code
 
-
-  constructor(){
-
-
+  constructor() {
     // Synchronous example 1 with blocking code
-    console.log("Setup for Pooja start");
-    debugger;
+    console.log('Setup for Pooja start');
 
     // this.getPoojaEquipmentsExample1();
     // this.getPoojaEquipmentsExample2();
-
 
     // this.getPoojaEquipmentsExample2().then((response) =>{
     //   console.log("equipments is purchased");
@@ -32,30 +26,24 @@ export class Learning {
     // const response  = this.getPoojaEquipmentsExample2();
 
     const response = this.getPoojaEquipmentsExample2().subscribe({
-      next: (response) =>{
-        debugger;
-        console.log("response", response)
+      next: (response) => {
+        console.log('response', response);
       },
-      error: (error) =>{
-        console.log("error", error)
-      }
-    })
-    
+      error: (error) => {
+        console.log('error', error);
+      },
+    });
 
-    debugger;
-    console.log("Setup is done for Pooja, person is went for equipments purchasing");
-
+    console.log('Setup is done for Pooja, person is went for equipments purchasing');
   }
 
-
-  async processPurchaseTask(){
-    const response  = await this.getPoojaEquipmentsExample2();
+  async processPurchaseTask() {
+    const response = await this.getPoojaEquipmentsExample2();
   }
 
-
-  getPoojaEquipmentsExample1(){
+  getPoojaEquipmentsExample1() {
     let purchase = '';
-    while (true){
+    while (true) {
       // pooja equipments is being purchased
       purchase = 'abcd';
     }
@@ -63,14 +51,14 @@ export class Learning {
     return purchase;
   }
 
-  getPromiseResult(): Promise<string>{
-    return new Promise((resolve, reject)=>{
-      resolve("Vegetables is being purchased");
-      reject("Some error is occurred during purchase of items")
-    } )
+  getPromiseResult(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      resolve('Vegetables is being purchased');
+      reject('Some error is occurred during purchase of items');
+    });
   }
 
-   getPoojaEquipmentsExample2(){
+  getPoojaEquipmentsExample2() {
     // setTimeout(() => {
     //   console.log("Pooja equipments purchased");
     // }, 5000);
@@ -85,35 +73,30 @@ export class Learning {
     //   console.log("There is some error to get vegetables", error);
     // })
 
-    
-
-     return new Observable((observer)=> {
+    return new Observable((observer) => {
       // observer.next("Item A is purchased");
       // observer.next("Item B is purchased");
       // observer.next("Item C is purchased");
 
-      fetch("https://example.com/getPurchasedItems").then((response) => response.json()).then((response)=>{
-      observer.next("Vegetables is purchased")
-    }).catch((error) => {
-      console.log("There is some error to get vegetables", error);
-    })
+      fetch('https://example.com/getPurchasedItems')
+        .then((response) => response.json())
+        .then((response) => {
+          observer.next('Vegetables is purchased');
+        })
+        .catch((error) => {
+          console.log('There is some error to get vegetables', error);
+        });
 
       // return of("Purchased")
-    })
-
-
-
+    });
   }
 
-
-  off(item: string): Observable<string>{
-
-    return new Observable((observer)=> {
-      observer.next("Item A is purchased");
-      observer.next("Item B is purchased")
+  off(item: string): Observable<string> {
+    return new Observable((observer) => {
+      observer.next('Item A is purchased');
+      observer.next('Item B is purchased');
 
       // return of("Purchased")
-    })
+    });
   }
-
 }
