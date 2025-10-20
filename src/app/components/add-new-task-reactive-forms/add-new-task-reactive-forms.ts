32d3f9b42d34729b1e474f2c8e0133576cede59b
Deepak Uniyal <uniyaldeepak49@gmail.com>
2025-10-20 15:03:26 +0530

    Removed Unnecessary code, Updated Angular version, fixes

--- src/app/components/add-new-task-reactive-forms/add-new-task-reactive-forms.ts
+++ src/app/components/add-new-task-reactive-forms/add-new-task-reactive-forms.ts
@@ -32,13 +32,12 @@ export class AddNewTaskReactiveForms {
 
   constructor() {
     effect(() => {
-      this.id.set(this.task()()?.id ?? 0);
+      this.id.set(this.task()()?.id ? (this.task()()?.id as number) : 0);
 
       if (this.id()) {
         // Populate values
         this.taskFormGroup.setValue(this.task()() as Task);
       } else {
-        debugger;
         if (!this.taskFormGroup.pristine) {
           this.taskFormGroup.reset();
         }
